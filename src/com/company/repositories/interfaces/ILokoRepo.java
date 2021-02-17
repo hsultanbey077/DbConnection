da7697574988bcaecae624e0a07ac1e5ec5f596e
batyr23 <shutenovbatyrkhan@gmail.com>
2021-02-17 17:52:19 +0600

    i added ilokorepo 2021/02/17

---
+++ src/com/company/repositories/interfaces/ILokoRepo.java
@@ -0,0 +1,15 @@
+package com.company.repositories.interfaces;
+
+import com.company.entities.Locomotive;
+import com.company.entities.Train;
+
+import java.util.List;
+
+public interface ILokoRepo {
+    boolean createLocomotive(Locomotive locomotive);
+    Locomotive getLocomotive(int id);
+    List<Locomotive> getAllLocomotives();
+
+
+
+}
